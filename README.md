76ed4ef30ca16c03fcafe567eff1ff27cbaab5df
Matthew Rogers <matt@matthewrogers.org>
2020-05-25 16:27:55 -0400

    Initial

---
+++ README.md
@@ -0,0 +1,56 @@
+# layer2_exporter
+
+Layer2_exporter is a [Prometheus](https://prometheus.io) exporter with layer2 network scanning.  This a not your regular network inventory, it detects devices that are silent and do not respond to pings or scans. It's written in go, and works on Linux/MacOS.
+
+## Installation
+
+Use go to install to your gopath.
+
+```bash
+go get -u github.com/RamboRogers/layer2_exporter 
+```
+
+## Usage
+Compile using go, and execute with a CIDR block to begin scanning.  Scanning a /24 takes about 10-15 minutes. Scans will continue on a cycle forever. 
+
+The default metrics port is TCP/HTTP 9095.  If running locally [127.0.0.1:9095/metrics](127.0.0.1:9095/metrics)
+```bash
+#BUILDING
+cd ~/go/src/RamboRogers/layer2_exporter
+go build
+
+#RUNNING
+./layer2_exporter 192.168.1.0/24
+
+#RUN ON CUSTOM PORT
+./layer2_exporter 192.168.1.0/24 :9999
+
+#RUN ON LOCAL INTERFACE
+./layer2_exporter 192.168.1.0/24 127.0.0.1:9999
+```
+## Prometheus Setup
+Setting up in your Prometheus is easy. Just use this example, append to your existing Prometheus config and adjust the target to match where your exporter is running.
+
+```bash
+- job_name: layer2scan
+  scrape_interval: 60s
+  metrics_path: /metrics
+  static_configs:
+    - targets:
+       - 192.168.1.240:9095
+```
+
+
+## Grafana Dashboard
+Here is an example dashboard. You can import to your Grafana. This was made on Grafana version 7.
+>![Example Dashboard](ExampleGrafana.png)
+
+[Download Dashboard](grafana/Layer2Discovery-Example.json)
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ layer2_exporter.go
@@ -0,0 +1,158 @@
+/*
+
+   Purpose:  This is a layer2 prometheus arp exporter.
+
+   Author:   Matthew Rogers
+   Date:     2020-05-25
+   License:  GPLv2
+
+   Revision: v1.0.0
+
+*/
+
+package main
+
+import (
+	"bytes"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"net/http"
+	"time"
+	"github.com/apparentlymart/go-cidr/cidr"
+	"github.com/j-keck/arping"
+	"github.com/klauspost/oui"
+)
+
+//Globals
+var completeScan = make(map[string]string)
+var targetSubnet = "UNKNOWN"
+var ouiURL = "http://standards-oui.ieee.org/oui.txt"
+
+//scan should be running constantly.... once done the metrics table should be updated.
+func showMetrics(w http.ResponseWriter, req *http.Request) {
+
+	//build oui
+	db, err := oui.OpenStaticFile("oui.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//target CIDR
+	fmt.Fprintf(w, "layer2targetCidr{targetCidr=\"%s\"} 1\n", targetSubnet)
+	//host count
+	fmt.Fprintln(w, "layer2hostCount", len(completeScan))
+
+	//{mac} ipaddress
+	for key, value := range completeScan {
+		
+		var ouiOut = ""
+
+		//lookup the OUI on render
+		entry, err := db.Query(value)
+		if err == oui.ErrNotFound {
+			ouiOut = "UNKNOWN"
+		} else if err != nil {
+			panic(err)
+		} else {
+			ouiOut = entry.Manufacturer
+		}
+
+
+		fmt.Fprintf(w, "layer2host{ip=\"%s\", mac=\"%s\", oui=\"%s\"} 1\n", key,value,ouiOut)
+	}
+}
+
+func main() {
+	//Let's create our maps!
+	
+	workingScan := make(map[string]string)
+
+	//take arguments
+	flag.Parse()
+	scanCidr := flag.Arg(0)
+	targetSubnet = scanCidr
+	listenAddress := ":9095"
+	
+	//custom port and interface
+	if len(flag.Arg(1)) > 0 {
+		listenAddress = flag.Arg(1)
+	}
+
+
+	http.HandleFunc("/metrics", showMetrics)
+	
+	//Server is parallel
+	go http.ListenAndServe(listenAddress, nil)
+	fmt.Println("Server is up....")
+
+	time.Sleep(30 * time.Second)
+	//check for bad CIDR
+	_, ipv4Net, err := net.ParseCIDR(scanCidr)
+	if err != nil {
+		fmt.Println("Provide a valid CIDR Subnet as an argument")
+		fmt.Println("Example: go_network_scanner 192.168.0.1/24")
+		log.Fatal(err)
+	}
+
+	fmt.Println("Net:", ipv4Net)
+	startAddress, endAddress := cidr.AddressRange(ipv4Net)
+	fmt.Println("Start/End:", startAddress, endAddress)
+	fmt.Println("Count:", cidr.AddressCount(ipv4Net))
+
+	//Scan Loop
+	//	scan INTO working scan which starts with zero values
+	//	once scan done, move working into finished, and zero out working
+	//	restart scan
+	for {
+		startTime := time.Now()
+		scanSubnet(startAddress, endAddress, workingScan)
+
+		//scan finished
+		println("*SCAN DONE*")
+		for key, value := range workingScan {
+			println("key:",key,"value:",value)
+		}
+
+		//wipe the prior scan
+		for key := range completeScan {
+			delete(completeScan, key)
+		}
+
+		//copy to it
+		for key, value := range workingScan {
+			completeScan[key] = value
+		}
+
+		//wipe working scan
+		for key := range workingScan {
+			delete(workingScan, key)
+		}
+		finishedTime := time.Now()
+		fmt.Println("Total Scan Time:", finishedTime.Sub(startTime))
+		
+		//put a pause in scanning
+		time.Sleep(10 * time.Second)
+	}
+
+}
+
+//this is the scan function
+func scanSubnet(startAddress net.IP, endAddress net.IP, workingScan map[string]string) {
+
+	//Go Through IP Address Range
+	for ipAddress := startAddress; bytes.Compare(ipAddress, cidr.Inc(endAddress)) != 0; ipAddress = cidr.Inc(ipAddress) {
+
+		arping.SetTimeout(100 * time.Millisecond)
+
+		macAddress, _, _ := arping.Ping(ipAddress)
+
+		if len(macAddress) > 0 {
+			//fmt.Println(ipAddress, macAddress)
+			workingScan[ipAddress.String()] = macAddress.String()
+		}
+
+	}
+
+}
